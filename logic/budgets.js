ac4fa77a0be15e3aa4d80a84e4eafaf1b8157b9b
Sebastian Pekarek <mail@sebbo.net>
2017-10-30 15:10:20 +0100

    Budgets.update()

--- logic/budgets.js
+++ logic/budgets.js
@@ -134,12 +134,81 @@ class BudgetLogic extends BaseLogic {
 			else if(k === 'document') {
 				sql.include[0].include[0].where = {id};
 			}
+			else if(k === 'hidden') {
+				sql.where = {
+					hidden: id === '1' || id === 'true'
+				};
+			}
 			else {
 				throw new ErrorResponse(400, 'Unknown filter `' + k + '`!')
 			}
 		});
 
 		return this.getModel().findAll(sql);
+	}
+
+	static update (model, body, options) {
+		const DatabaseHelper = require('../helpers/database');
+
+		if(body.name !== undefined) {
+			model.name = body.name;
+		}
+		if (!model.name) {
+			throw new ErrorResponse(400, 'Budget requires attribute `name`…', {
+				attributes: {
+					name: 'Is required!'
+				}
+			});
+		}
+		if (model.name.length > 255) {
+			throw new ErrorResponse(400, 'Attribute `Budget.name` has a maximum length of 255 chars, sorry…', {
+				attributes: {
+					name: 'Is too long, only 255 characters allowed…'
+				}
+			});
+		}
+
+		if(body.goal !== undefined) {
+			model.goal = parseInt(body.goal, 10) || null;
+		}
+		if(body.hidden !== undefined) {
+			model.hidden = !!body.hidden;
+		}
+
+		if(!body.categoryId) {
+			return model.save();
+		}
+
+		return DatabaseHelper.get('category')
+			.find({
+				where: {id: body.categoryId},
+				attributes: ['id'],
+				include: [{
+					model: DatabaseHelper.get('document'),
+					attributes: [],
+					include: [{
+						model: DatabaseHelper.get('user'),
+						attributes: [],
+						where: {
+							id: options.session.userId
+						}
+					}]
+				}]
+			})
+			.then(function (categoryModel) {
+				if (!categoryModel) {
+					throw new ErrorResponse(400, 'Not able to update budget: linked category not found.');
+				}
+
+				model.categoryId = categoryModel.id;
+				return model.save();
+			})
+			.then(function (model) {
+				return {model};
+			})
+			.catch(e => {
+				throw e;
+			});
 	}
 
 	static delete () {
